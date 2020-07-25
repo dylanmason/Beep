294c28481a54b2a4417cf4923215299ff79eb3ff
Banks Nussman <banks@nussman.us>
2020-07-25 18:46:38 -0400

    made App.js a class, no longer a component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, StatusBar, AsyncStorage } from 'react-native';
@@ -13,7 +13,6 @@ import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { ThemeContext } from './utils/theme-context';
 import { UserContext } from './utils/UserContext.js';
 import * as SplashScreen from 'expo-splash-screen';
-import { registerForPushNotificationsAsync } from './utils/Notifications.js';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
 
 const Stack = createStackNavigator();
@@ -28,81 +27,100 @@ async function startSplash() {
     }
 }
 
-export default function App() {
-    startSplash();
+export default class App extends Component {
 
-    const [user, setUser] = React.useState();
-    const [theme, setTheme] = React.useState('light');
-    const [isLoading, setIsLoading] = React.useState(true);
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: {},
+            theme: "light"
+        };
+    }
 
-    const toggleTheme = () => {
-        const nextTheme = theme === 'light' ? 'dark' : 'light';
-        setTheme(nextTheme);
+    toggleTheme = () => {
+        const nextTheme = this.state.theme === 'light' ? 'dark' : 'light';
+        this.setState({theme: nextTheme});
         AsyncStorage.setItem('@theme', nextTheme);
     };
+
+    setUser = (user) => {
+        this.setState({user: user});
+    }
     
     //If we haven't deturmined an initial screen, run this code
     //We DONT want to run this code if we already know what screen to load
-    if (initialScreen == null) {
-        //When App loads initially, get token from AsyncStorage
-        AsyncStorage.multiGet(['@user', '@theme']).then((result) => {
-            //if a user is defined
-            if(result[0][1]) {
-                //Register for Expo Push Notifications
-                registerForPushNotificationsAsync();
-                //Because user is logged in, send them to Main initially
-                initialScreen = "Main";
-                //Take user from AsyncStorage and put it in our context
-                setUser(JSON.parse(result[0][1]));
-            }
-            else {
-                //This mean no one is logged in, send them to login page initally
-                initialScreen = "Login";
-            }
+    componentDidMount() {
+        startSplash();
 
-            if(result[1][1]) {
-                //re-render may happen, if a re-render happens, this code will not run agian because
-                //initialScreen has been defined. 
-                setTheme(result[1][1]);
-            }
+        if (initialScreen == null) {
+            //When App loads initially, get token from AsyncStorage
+            AsyncStorage.multiGet(['@user', '@theme']).then((result) => {
+                let sUser = null;
+                let sTheme = this.state.theme;
+                //if a user is defined
+                if(result[0][1]) {
+                    //Because user is logged in, send them to Main initially
+                    initialScreen = "Main";
+                    //Take user from AsyncStorage and put it in our context
+                    sUser = JSON.parse(result[0][1]);
+                }
+                else {
+                    //This mean no one is logged in, send them to login page initally
+                    initialScreen = "Login";
+                }
 
-            setIsLoading(false);
-          }, (error) => {
-            //AsyncStorage could not get data from storage
-            console.log("[App.js] [AsyncStorage] ", error);
-        });
+                if(result[1][1]) {
+                    //re-render may happen, if a re-render happens, this code will not run agian because
+                    //initialScreen has been defined. 
+                    sTheme = result[1][1];
+                }
+                
+                this.setState({
+                    user: sUser,
+                    theme: sTheme
+                });
+              }, (error) => {
+                //AsyncStorage could not get data from storage
+                console.log("[App.js] [AsyncStorage] ", error);
+            });
+        }
     }
 
-    if (isLoading) {
-        //TODO: this renders 3 times >:(
-        console.log("Loading...");
-        return null;
+    render () {
+        if (!initialScreen) {
+            console.log("[App.js] Rendering App with Intial Screen: ", initialScreen);
+            return null;
+        }
+
+        console.log("[App.js] Rendering App with Intial Screen: ", initialScreen);
+        let user = this.state.user;
+        let setUser = this.setUser;
+
+        let theme = this.state.theme;
+        let toggleTheme = this.toggleTheme;
+
+        return (
+            <>
+            <IconRegistry icons={EvaIconsPack} />
+            <UserContext.Provider value={{user, setUser}}>
+                <ThemeContext.Provider value={{theme, toggleTheme}}>
+                    <ApplicationProvider {...eva} theme={{ ...eva[this.state.theme], ...beepTheme }}>
+                        <Layout style={styles.statusbar}>
+                            <StatusBar barStyle={(this.state.theme === 'light' ? 'dark' : 'light') + "-content"} />
+                        </Layout>
+                        <NavigationContainer>
+                            <Stack.Navigator initialRouteName={initialScreen} screenOptions={{ headerShown: false }} >
+                                <Stack.Screen name="Login" component={LoginScreen} />
+                                <Stack.Screen name="Register" component={RegisterScreen} />
+                                <Stack.Screen name="Main" component={MainScreen} />
+                            </Stack.Navigator>
+                        </NavigationContainer>
+                    </ApplicationProvider>
+                </ThemeContext.Provider>
+            </UserContext.Provider>
+            </>
+        );
     }
-
-    //Loading is done! Render our main app
-    console.log("[App.js] Rendering App with Intial Screen: ", initialScreen);
-
-    return (
-        <>
-        <IconRegistry icons={EvaIconsPack} />
-        <UserContext.Provider value={{user, setUser}}>
-            <ThemeContext.Provider value={{ theme, toggleTheme }}>
-                <ApplicationProvider {...eva} theme={{ ...eva[theme], ...beepTheme }}>
-                    <Layout style={styles.statusbar}>
-                        <StatusBar barStyle={(theme === 'light' ? 'dark' : 'light') + "-content"} />
-                    </Layout>
-                    <NavigationContainer>
-                        <Stack.Navigator initialRouteName={initialScreen} screenOptions={{ headerShown: false }} >
-                            <Stack.Screen name="Login" component={LoginScreen} />
-                            <Stack.Screen name="Register" component={RegisterScreen} />
-                            <Stack.Screen name="Main" component={MainScreen} />
-                        </Stack.Navigator>
-                    </NavigationContainer>
-                </ApplicationProvider>
-            </ThemeContext.Provider>
-        </UserContext.Provider>
-        </>
-    );
 }
 
 const styles = StyleSheet.create({
